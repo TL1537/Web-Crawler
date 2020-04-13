e2ef500e70598ef1d416126ee6f269c275cba317
TL1537 <1910087029@qq.com>
2020-04-13 15:43:51 +0800

    master

--- GetUniversity.py
+++ GetUniversity.py
@@ -14,17 +14,18 @@ def getHTMLText(url):
 def fillUnivList(ulist, html):
     soup = BeautifulSoup(html, "html.parser") #指定Beautiful的解析器为“html.parser”
     #通过搜索网页源代码发现，大学排名的具体信息保存在 tbody 标签中
-    for tr in soup.find('tbody').children: # .find是搜索并只返回一个结果；.children是儿子节点
+    for trs in soup.find('tbody').children: # .find是搜索并只返回一个结果；.children是儿子节点
         # isinstance:判断一个对象是否是一个已知的类型，类似于type()
         # type()不考虑子类是父类的一种类型，不考虑继承关系
         # isinatance()认为子类是父类的一种类型，考虑继承关系
         # bs4.element.Tag
         # bs4是包，element是模块，Tag是类名
-        # tag是‘bs4.element.Tag’的实例对象，或者说tag的数据类型是bs4.element.Tag
+        # [tag是‘bs4.element.Tag’的实例对象，或者说tag的数据类型是bs4.element.Tag]
         # isinstance(object, classinfo)其中object是实例对象，classinfo可以是直接或间接类名、基本类型或者由它们组成的元组。
-        # 使用方法：如果对象的类型与参数二的类型（classinfo）相同则返回 True，否则返回 False。
-        if isinstance(tr, bs4.element.Tag): 
-            tds = tr('td') #？
+        # [使用方法：如果对象的类型与参数二的类型（classinfo）相同则返回 True，否则返回 False。]
+        print(type(trs))
+        if isinstance(trs, bs4.element.Tag): 
+            tds = trs('td') #？
             ulist.append([tds[0].string, tds[1].string, tds[3].string]) # ？
  
 def printUnivList(ulist, num):
---
+++ Getdouban.py
@@ -0,0 +1,69 @@
+## find()
+import requests
+from bs4 import BeautifulSoup
+import bs4
+
+
+def getHtml(url, my):
+    try:
+        r = requests.get(url, timeout=30, headers = my)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        print('爬取失败')
+
+def Fillcase(html, movielist):
+    soup = BeautifulSoup(html, 'html.parser')
+    for divs in soup.find('ol').children: #观察源代码可知，ol包含了所有的电影信息
+        if isinstance(divs, bs4.element.Tag):
+            lis = divs('span') #电影名包含在span中
+            movielist.append(lis[0].string)
+
+def show(num, movielist):
+    for i in range(num):
+        print(movielist[i])
+
+def master():
+    url = 'https://movie.douban.com/top250'
+    my = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
+    html = getHtml(url, my)
+    movielist = []
+    Fillcase(html, movielist)
+    show(20, movielist)
+
+master()
+
+## findall()
+# import requests
+# from bs4 import BeautifulSoup
+# import bs4
+
+# def getHtml(url, my):
+#     try:
+#         r = requests.get(url, timeout=30, headers = my)
+#         r.raise_for_status()
+#         r.encoding = r.apparent_encoding
+#         return r.text
+#     except:
+#         print('爬取失败')
+
+# def Fillcase(html, movielist):
+#     soup = BeautifulSoup(html, 'html.parser')
+#     for divs in soup.find_all('div', class_ = 'hd'):
+#         lis = divs('span') 
+#         movielist.append(lis[0].string)
+
+# def show(num, movielist):
+#     for i in range(num):
+#         print(movielist[i])
+
+# def master():
+#     url = 'https://movie.douban.com/top250'
+#     my = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
+#     html = getHtml(url, my)
+#     movielist = []
+#     Fillcase(html, movielist)
+#     show(20, movielist)
+
+# master()
