67e3671cf55505e97d3efd841c4162f18e5bf3fa
TL1537 <1910087029@qq.com>
2020-04-14 11:12:51 +0800

    master

--- Moviechoose.py
+++ Moviechoose.py
@@ -3,7 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 import bs4
 
-def getHtml(url, my):
+def getHtml():
+    url = 'https://movie.douban.com/top250'
+    my = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
     try:
         r = requests.get(url, timeout=30, headers = my)
         r.raise_for_status()
@@ -12,52 +14,74 @@ def getHtml(url, my):
     except:
         print('爬取失败')
 
-def moviecase(html, movielist, num):
-    soup = BeautifulSoup(html, 'html.parser')
+def moviecase(movielist):
     for divs in soup.find_all('div', class_ = 'hd'):
         movieli = divs('span') 
         movielist.append(movieli[0].string)
 
-def starcase(html, starlist, num):
-    soup = BeautifulSoup(html, 'html.parser')
+def foreign_moviecase(foreign_movielist):
+    for divs in soup.find_all('div', class_ = 'hd'):
+        for acs in divs.find_all('a', class_ = ''):
+            foreign_movieli = acs('span')
+        if len(foreign_movieli) == 3:
+            foreign_movielist.append((foreign_movieli[1].string)[3:])
+        else:
+            foreign_movielist.append('无外语名')
+
+def starcase(starlist):
     for ps in soup.find_all('div', class_ = 'star'):
         starlis = ps('span') 
         starlist.append(starlis[1].string)
 
-def quotecase(html, quotelist, num):
-    soup = BeautifulSoup(html, 'html.parser')
+def quotecase(quotelist):
     for ps in soup.find_all('p', class_ = 'quote'):
         quotelis = ps('span') 
         quotelist.append(quotelis[0].string)
 
 def version():
-    print('Getdouban-2.2')
+    print('Getdouban-2.4')
 
-url = 'https://movie.douban.com/top250'
-my = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
-html = getHtml(url, my)
+
+html = getHtml()
+soup = BeautifulSoup(html, 'html.parser')
 quotelist = []
 movielist = []
 starlist = []
+foreign_movielist = []
+
 
 print('1.英文名 ' + '2.其他译名 ' + '3.是否可播放 ' + '4.演员 ' + '5.时间地区和标签 ' + '6.评分 ' + '7.经典语录 ' + '8.显示版本')
 choose = ['0']
 count = eval(input('您共需要几项功能：'))
 for i in range(count):
     choose.append(input('请选择你需要的功能：'))
-num = eval(input('请输入要显示的电影数量'))
+
+#------------------------
+# tempcount = '1'
+# for i in range(count):
+#     choose.append(input('请选择你需要的第' + tempcount + '项功能：'))
+#     int(tempcount)
+#     tempcount = tempcount + 1
+#     str(tempcount)
+#-----------------------
+
+num = eval(input('请输入要显示的电影数量：'))
 for time in range(num):
     if '0' in choose:
-        moviecase(html, movielist, num)
-        print(movielist[time])
+        moviecase(movielist)
+        print(movielist[time] + ' ', end='')
+
+    if '1' in choose:
+        foreign_moviecase(foreign_movielist)
+        print(foreign_movielist[time] + ' ', end='')
 
     if '6' in choose:
-        starcase(html, starlist, num)
-        print(starlist[time] + '分')
+        starcase(starlist)
+        print(starlist[time] + '分 ', end='')
 
     if '7' in choose:
-        quotecase(html, quotelist, num)
-        print(quotelist[time])
+        quotecase(quotelist)
+        print(quotelist[time] + ' ')
 
-    if '8' in choose:
-        version()
+if '8' in choose:
+    version()
